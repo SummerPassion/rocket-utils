c4ea1cf0e1e10e017d1c58f7c009978d551c8bdb
SummerPassion <695998135@qq.com>
2022-02-25 17:35:55 +0800

    init commit

---
+++ src/Utils.php
@@ -0,0 +1,34 @@
+<?php
+declare (strict_types=1);
+
+/**
+ * Author:
+ *
+ *   ┏┛ ┻━━━━━┛ ┻┓
+ *   ┃　　　━　　  ┃
+ *   ┃　┳┛　  ┗┳  ┃
+ *   ┃　　　-　　  ┃
+ *   ┗━┓　　　┏━━━┛
+ *     ┃　　　┗━━━━━━━━━┓
+ *     ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+ *       ┗━┻━┛   ┗━┻━┛
+ * DateTime: 2022-02-25 16:43:00
+ */
+
+namespace rocket;
+
+/**
+ * 工具类
+ * Class Utils
+ * @package rocket
+ * create_at: 2022-02-25 16:43:49
+ * update_at: 2022-02-25 16:43:49
+ */
+class Utils
+{
+
+    public static function redis()
+    {
+
+    }
+}
---
+++ src/exception/RedisException.php
@@ -0,0 +1,31 @@
+<?php
+declare (strict_types=1);
+
+/**
+ * Author:
+ *
+ *   ┏┛ ┻━━━━━┛ ┻┓
+ *   ┃　　　━　　  ┃
+ *   ┃　┳┛　  ┗┳  ┃
+ *   ┃　　　-　　  ┃
+ *   ┗━┓　　　┏━━━┛
+ *     ┃　　　┗━━━━━━━━━┓
+ *     ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+ *       ┗━┻━┛   ┗━┻━┛
+ * DateTime: 2022-02-25 17:03:54
+ */
+
+namespace rocket\exception;
+
+use Exception;
+
+/**
+ * Class RedisException
+ * @package rocket\exception
+ * create_at: 2022-02-25 17:04:32
+ * update_at: 2022-02-25 17:04:32
+ */
+class RedisException extends Exception
+{
+
+}
